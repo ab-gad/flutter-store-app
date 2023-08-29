27d031c6f36a9d466a15a72689f4275b65aa778a
ab-gad <philosopher.gad@gmail.com>
2023-08-29 13:34:19 +0300

    create font and values managers

---
+++ lib/presentation/fonts_manager.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+abstract class FontConstants {
+  static const fontFamily = "Montserrat";
+}
+
+abstract class FontWeights {
+  static const light = FontWeight.w300;
+  static const regular = FontWeight.w400;
+  static const medium = FontWeight.w500;
+  static const semiBold = FontWeight.w600;
+  static const bold = FontWeight.w700;
+}
+
+abstract class FontSizes {
+  static const s12 = 12;
+  static const s14 = 14;
+  static const s16 = 16;
+  static const s18 = 18;
+  static const s20 = 20;
+}
---
+++ lib/presentation/values_manager.dart
@@ -0,0 +1,19 @@
+///
+///This class is used as a margin, padding, ..etc constants holder
+///
+///For more Fine grained control you can separate each concern to
+///its separated class for ex. AppMargin, AppPadding, ..etc.
+///
+///This will allow you later to control specific padding value for example
+///without affecting the corresponding margin value, so you may hve p8 in
+///padding that hold a different value from the m8 in the margin class
+///
+abstract class AppValues {
+  static const v8 = 8;
+  static const v10 = 10;
+  static const v12 = 12;
+  static const v14 = 14;
+  static const v16 = 16;
+  static const v18 = 18;
+  static const v20 = 20;
+}

1189ae4876144f0749da218b54160cb5093a70d7
ab-gad <philosopher.gad@gmail.com>
2023-08-30 10:48:51 +0300

    create a rout manager & empty widget for each view

---
+++ lib/resources/routes_manager.dart
@@ -0,0 +1,8 @@
+abstract class RoutesManager {
+  static const splash = '/';
+  static const registration = '/registration';
+  static const login = '/login';
+  static const forgetPassword = '/forgetPassword';
+  static const main = '/main';
+  static const storeDetails = '/storeDetails';
+}

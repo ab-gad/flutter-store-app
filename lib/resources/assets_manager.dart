f7b84b4981b2e36cce16a5349ee2e8441c9bceec
ab-gad <philosopher.gad@gmail.com>
2023-08-31 15:33:10 +0300

    Add onboarding assets

--- lib/resources/assets_manager.dart
+++ lib/resources/assets_manager.dart
@@ -1,4 +1,12 @@
 abstract class AppImages {
   static const _imagesPath = "assets/images";
+
+  // ============[Splash View Images]=================
   static const splashLogo = "$_imagesPath/splash_logo.png";
+
+  // ============[Onboarding View Images]=================
+  static const onboardingLogoSvg1 = "$_imagesPath/onboarding_logo1.svg";
+  static const onboardingLogoSvg2 = "$_imagesPath/onboarding_logo2.svg";
+  static const onboardingLogoSvg3 = "$_imagesPath/onboarding_logo3.svg";
+  static const onboardingLogoSvg4 = "$_imagesPath/onboarding_logo4.svg";
 }

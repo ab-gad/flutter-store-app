6b132350d759a0ffc601615671036138cc488463
ab-gad <philosopher.gad@gmail.com>
2023-09-01 08:55:10 +0300

    Onboarding 1, Start with "bad approach"

--- lib/presentation/onboarding/onboarding_view.dart
+++ lib/presentation/onboarding/onboarding_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_store_app/resources/assets_manager.dart';
+import 'package:flutter_store_app/resources/string_manager.dart';
 
 class OnboardingView extends StatefulWidget {
   const OnboardingView({super.key});
@@ -8,12 +10,52 @@ class OnboardingView extends StatefulWidget {
 }
 
 class _OnboardingViewState extends State<OnboardingView> {
+  final List<SliderObject> _sliderObjcets = [
+    SliderObject(
+        title: StringManager.sliderTitle1,
+        subTitle: StringManager.sliderSubTitle1,
+        imageUrl: AppImages.onboardingLogoSvg1),
+    SliderObject(
+        title: StringManager.sliderTitle2,
+        subTitle: StringManager.sliderSubTitle2,
+        imageUrl: AppImages.onboardingLogoSvg2),
+    SliderObject(
+        title: StringManager.sliderTitle3,
+        subTitle: StringManager.sliderSubTitle3,
+        imageUrl: AppImages.onboardingLogoSvg3),
+    SliderObject(
+        title: StringManager.sliderTitle4,
+        subTitle: StringManager.sliderSubTitle4,
+        imageUrl: AppImages.onboardingLogoSvg4),
+  ];
+
+  PageController _pageController = PageController();
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Welcome ya Zmely'),
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: [Container()],
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
+}
+
+class SliderObject {
+  final String title;
+  final String subTitle;
+  final String imageUrl;
+
+  SliderObject({
+    required this.title,
+    required this.subTitle,
+    required this.imageUrl,
+  });
 }
--- lib/resources/string_manager.dart
+++ lib/resources/string_manager.dart
@@ -1,4 +1,16 @@
 abstract class StringManager {
   static const pageNotFound = "Page Not Found";
   static const pageNotFoundMsg = "The page you are looking for is not found";
+
+  // ================[Onboarding View]==============
+  static const sliderTitle1 = "GREATNESS IS WAITING FOR U";
+  static const sliderTitle2 = "GREATNESS IS STILL WAITING FOR U";
+  static const sliderTitle3 = "GREATNESS IS STILL WAITING FOR U";
+  static const sliderTitle4 = "GREATNESS IS HERE";
+  static const sliderSubTitle1 = "You are just 3 step away from greatness";
+  static const sliderSubTitle2 =
+      "Common!, You are just 2 step away from greatness";
+  static const sliderSubTitle3 =
+      "You will make it, Only 1 step away from greatness";
+  static const sliderSubTitle4 = "Foooooooooooooooool XD";
 }

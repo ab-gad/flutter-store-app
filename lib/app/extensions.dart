79ce7c981150d2dc4f8b4a5a94d61be8ac047402
ab-gad <philosopher.gad@gmail.com>
2023-09-12 16:55:24 +0300

    create non null extension

---
+++ lib/app/extensions.dart
@@ -0,0 +1,40 @@
+// Null-protect extensions
+extension NonNullString on String? {
+  String orEmpty() {
+    if (this == null) {
+      return "";
+    } else {
+      return this!;
+    }
+  }
+}
+
+extension NonNullInt on int? {
+  int orZero() {
+    if (this == null) {
+      return 0;
+    } else {
+      return this!;
+    }
+  }
+}
+
+extension NonNullBool on bool? {
+  bool orFalse() {
+    if (this == null) {
+      return false;
+    } else {
+      return this!;
+    }
+  }
+}
+
+extension NonNullList on List? {
+  List orEmptyList() {
+    if (this == null) {
+      return [];
+    } else {
+      return this!;
+    }
+  }
+}
